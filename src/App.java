021b3b51a20978b9922888726a3dd24bf55322a7
savlu <savanhlu06@yahoo.com>
2016-03-02 15:23:06 -0500

    savanh changed things

--- src/App.java
+++ src/App.java
@@ -3,14 +3,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 1;
-		int y = 2;
-		int sum;
-		
-		sum = a + y;
-		
-		System.out.println("The sum is: " + sum + ".");
-		
+		 System.out.print("hi"); //from savanh
 	}
 
 }
