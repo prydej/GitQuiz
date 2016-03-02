30982224a26c31d5f9fba3af142a541116db4cbd
kristins <kristins@LB373-08-4.erau.edu>
2016-03-02 15:31:42 -0500

    stephen # 2

--- src/App.java
+++ src/App.java
@@ -6,6 +6,16 @@ public class App {
 		 System.out.print("hi"); //from savanh
 		 
 		 char a, b, c; // stephen #1
+		 
+		 
+		 // stephen #2
+		 int i, j;
+		 for (i=0; i<5; i++){
+			 for (j=0; j<5; j++){
+				 System.out.println(" * ");
+			 }
+		 }// end stephen #2
+		 
 	}
 
 }
