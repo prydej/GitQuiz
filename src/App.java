f634dbe4200019c671bbf85b3856a327ba288557
Julian Pryde <prydej@my.erau.edu>
2016-03-02 15:18:23 -0500

    "Add 1 and 2."

--- src/App.java
+++ src/App.java
@@ -3,7 +3,14 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 1;
+		int y = 2;
+		int sum;
+		
+		sum = a + y;
+		
+		System.out.println("The sum is: " + sum + ".");
+		
 	}
 
 }
